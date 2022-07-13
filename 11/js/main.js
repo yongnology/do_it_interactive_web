189daba03741e4f6bd2847eb6e2b24a81ca21af8
yongnology <cassius20@naver.com>
2022-07-13 21:34:23 +0900

    12 뮤직 프레이어 제작하기(템플릿 준비)

--- 11/js/main.js
+++ 11/js/main.js
@@ -1,7 +1,9 @@
 // 모든 article 요소를 변수 items에 저장
 const items = document.querySelectorAll("article");
+const aside = document.querySelector("aside");
+const close = aside.querySelector("span")
 
-// article 요소의 개수만큼 반복
+// items 요소의 개수만큼 반복
 for(let el of items) {
     //현재 반복하는 article 요소에 mouseenter 이벤트 발생
     el.addEventListener("mouseenter",e=>{
@@ -12,5 +14,29 @@ for(let el of items) {
     el.addEventListener("mouseleave",e=> {
         // 자식 요소인 video 일시 정지
         e.currentTarget.querySelector("video").pause();
+    });
+
+    //현재 반복돌고 있는 article요소에 click 이벤트 발생 시
+    el.addEventListener("click", e=>{     
+        //제목과 본문의 내용, 그리고 video요소의 src값을 변수에 저장  
+        let tit = e.currentTarget.querySelector("h2").innerText;
+        let txt = e.currentTarget.querySelector("p").innerText;
+        let vidSrc = e.currentTarget.querySelector("video").getAttribute("src");
+
+        //aside 요소 안쪽의 콘텐츠에 위의 변수 내용을 적용
+        aside.querySelector("h1").innerText = tit;
+        aside.querySelector("p").innerText = txt;
+        aside.querySelector("video").setAttribute("src", vidSrc);
+
+        //aside 요소 안쪽의 동영상을 재생하고 aside요소에 on을 붙여 팝업 패널 활성화
+        aside.querySelector("video").play();
+        aside.classList.add("on");
+    });
+
+    //닫기 버튼 클릭 시 
+    close.addEventListener("click", ()=>{
+        //aside 요소에 클래스 on을 제거해 비활성화 하고 안쪽의 영상을 재생중지
+        aside.classList.remove("on");
+        aside.querySelector("video").pause();
     });
 }
