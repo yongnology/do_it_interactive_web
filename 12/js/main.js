d23e4404b3254a8118b68b4d1e16545e520e4059
yongnology <cassius20@naver.com>
2022-07-16 22:40:05 +0900

    12-8단계 컨트롤 패널 버튼으로 음악을 재생 정지하기

--- 12/js/main.js
+++ 12/js/main.js
@@ -13,4 +13,33 @@ for(let el of lists) {
     /* 배경 이미지 추가 */
     pic.style.backgroundImage = `url(img/member${i+1}.jpg)`;
     i++;
+
+    // 각 article 요소 안쪽의 재생, 정지, 처음부터 재생 버튼을 변수에 저장
+    let play = el.querySelector(".play");
+    let pause = el.querySelector(".pause");
+    let load = el.querySelector(".load");
+
+    // play 버튼 클릭 시
+    play.addEventListener("click", e=> {
+        // play 버튼부터 .pic 요소까지 탐색한 뒤 클래스 on 추가하여 활성화
+        e.currentTarget.closest("article").querySelector(".pic").classList.add("on");
+        // play 버튼부터 audio 요소까지 탐색한 뒤 음악 재생
+        e.currentTarget.closest("article").querySelector("audio").play();
+    });
+
+    // pause 버튼 클릭 시
+    pause.addEventListener("click", e=> {
+        // pause 버튼부터 .pic 요소까지 탐색한 뒤 클래스 on 제거하여 비활성화
+        e.currentTarget.closest("article").querySelector(".pic").classList.remove("on");
+        // pause 버튼부터 audio 요소까지 탐색한 뒤 음악 정지
+        e.currentTarget.closest("article").querySelector("audio").pause();
+    });
+
+    // load 버튼 클릭 시
+    load.addEventListener("click", e=> {
+        // load 버튼부터 .pic 요소까지 탐색한 뒤 클래스 on 추가하여 활성화
+        e.currentTarget.closest("article").querySelector(".pic").classList.add("on");
+        e.currentTarget.closest("article").querySelector("audio").load();
+        e.currentTarget.closest("article").querySelector("audio").play();
+    });
 }
