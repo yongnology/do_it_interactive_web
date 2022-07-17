548e025eee45ceb8b6eb99e4db42e590cdcc6ca3
yongnology <cassius20@naver.com>
2022-07-17 22:49:22 +0900

    13-8단계 함수를 이용해 자바스크립트 코드 기능별로 개선하기

--- 13/js/main.js
+++ 13/js/main.js
@@ -1,3 +1,53 @@
+/* 방법1. 보수를 위한 함수 정렬하여 나열*/
+
+// 자주 수정할 수 있는 정보값들을 상단에 전역 변수로 설정
+const frame = "section";
+const box = "article";
+const speed = "0.5s";
+const activeClass = "on";
+const btn = document.querySelectorAll("main ul li");
+let grid; // 플러그인의 정보값이 담길 변수를 이곳에 전역으로 설정
+
+// 페이지 로드 이벤트
+// 이미지 소스를 활용한 모든 콘텐츠의 로딩이 완료되면
+window.addEventListener("load", ()=> {
+    init(); // 화면 초기화 함수 호출
+    filter(btn); // 정렬 버튼 기능의 함수 호출
+});
+
+// 화면 초기화 함수 정의
+function init() {
+    // 변수 grid에 담길 결과값이 다른 함수인 filter에도 활용되어야 하므로
+    // 전역 변수로 선언
+    grid = new Isotope(frame, {
+        itemSelector: box,
+        columnWidth: box,
+        transitionDuration: speed
+    });
+}
+
+// 정렬 버튼 기능의 함수 정의
+function filter(arr){ // 매개 변수 arr을 통해 반복하는 버튼 그룹을 인수로 전달
+    for(let el of arr) {
+
+        el.addEventListener("click", e=>{
+            e.preventDefault();
+
+            const sort = e.currentTarget.querySelector("a").getAttribute("href");
+
+            grid.arrange({
+                filter: sort
+            });
+            for(let el of arr) {
+                el.classList.remove(activeClass);
+            }
+
+            e.currentTarget.classList.add(activeClass);
+        })
+    }
+}
+
+/* 방법2. 풀이하면서 작성
 // 페이지 로드 이벤트
 window.addEventListener("load", ()=> {
     const grid = new Isotope("section", { // 배치할 요소를 감싸고 있는 부모 요소명
@@ -35,3 +85,4 @@ window.addEventListener("load", ()=> {
         })
     }
 });
+*/
