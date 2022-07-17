c39023092762203b961fea4ec5dec304a622a4ec
yongnology <cassius20@naver.com>
2022-07-17 22:35:25 +0900

    13-7단계 분류 기능 추가하기

--- 13/js/main.js
+++ 13/js/main.js
@@ -5,4 +5,33 @@ window.addEventListener("load", ()=> {
         columnWidth: "article",     // 너비값 구할 요소명
         transitionDuration: "0.5s"  // 화면 재배치 시 요소가 움직이는 속도
     });
+
+    // 클릭한 모든 버튼 변수에 저장
+    const btns = document.querySelectorAll("main ul li");
+
+    // 버튼의 개수만큼 반복해서
+    for(let el of btns) {
+        // 각 버튼에 클릭 이벤트 연결
+        el.addEventListener("click", e=> {
+            e.preventDefault();
+
+            // 변수 sort에 클릭한 대상의 자식인 a 요소의 href 속성값 저장
+            const sort = e.currentTarget.querySelector("a").getAttribute("href");
+
+            // grid에 저장된 결과값을 불러와 재정렬 기능 연결
+            grid.arrange ({
+                // 옵션값으로 sort 변숫값 지정
+                filter : sort
+            });
+
+            // 다시 모든 버튼의 개수만큼 반복해서
+            for(let el of btns) {
+                // 각 버튼의 클래스명 "on"을 제거해 비활성화
+                el.classList.remove("on");
+            }
+
+            // 클릭한 대상만 선택해서 클래스명 "on"을 추가해 활성화
+            e.currentTarget.classList.add("on");
+        })
+    }
 });
